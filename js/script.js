033be34951cdf36b46bc571d6b39685ed0e4b7fb
Thiem Quyen <88473863+dtquyen1199@users.noreply.github.com>
2024-06-26 01:37:54 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll(".navbar a");
+
+    for (const link of links) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+            const navbarHeight = document.querySelector(".navbar").offsetHeight;
+            const headerbarHeight = document.querySelector(".headerbar").offsetHeight;
+
+            window.scrollTo({
+                top: targetSection.offsetTop - headerbarHeight,
+                behavior: "smooth"
+            });
+        });
+    }
+});
